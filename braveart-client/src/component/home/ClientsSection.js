fb5c6e55afe90783c841b27ea8c3290dac615f71
Scott Latunski <latunsk4@gmail.com>
2019-06-18 13:10:06 -0400

    clean up unused images

--- braveart-client/src/component/home/ClientsSection.js
+++ braveart-client/src/component/home/ClientsSection.js
@@ -1,51 +1,51 @@
-import React from 'react';
+// import React from 'react';
+//
+// import client1 from "../../img/clients/1.png";
+// import client2 from "../../img/clients/2.png";
+// import client3 from "../../img/clients/3.png";
+// import client4 from "../../img/clients/4.png";
+// import client5 from "../../img/clients/5.png";
+//
+// const ClientsSection = () => (
+//     <div className="client-section spad">
+//         <div className="container">
+//             <div id="client-carousel" className="client-slider">
+//                 <Client
+//                     link = "#"
+//                     image = {client1}
+//                     imageAlt = "client1 thumbnail"
+//                 />
+//                 <Client
+//                     link = "#"
+//                     image = {client2}
+//                     imageAlt = "client2 thumbnail"
+//                 />
+//                 <Client
+//                     link = "#"
+//                     image = {client3}
+//                     imageAlt = "client3 thumbnail"
+//                 />
+//                 <Client
+//                     link = "#"
+//                     image = {client4}
+//                     imageAlt = "client4 thumbnail"
+//                 />
+//                 <Client
+//                     link = "#"
+//                     image = {client5}
+//                     imageAlt = "client5 thumbnail"
+//                 />
+//             </div>
+//         </div>
+//     </div>
+// );
 
-import client1 from "../../img/clients/1.png";
-import client2 from "../../img/clients/2.png";
-import client3 from "../../img/clients/3.png";
-import client4 from "../../img/clients/4.png";
-import client5 from "../../img/clients/5.png";
-
-const ClientsSection = () => (
-    <div className="client-section spad">
-        <div className="container">
-            <div id="client-carousel" className="client-slider">
-                <Client
-                    link = "#"
-                    image = {client1}
-                    imageAlt = "client1 thumbnail"
-                />
-                <Client
-                    link = "#"
-                    image = {client2}
-                    imageAlt = "client2 thumbnail"
-                />
-                <Client
-                    link = "#"
-                    image = {client3}
-                    imageAlt = "client3 thumbnail"
-                />
-                <Client
-                    link = "#"
-                    image = {client4}
-                    imageAlt = "client4 thumbnail"
-                />
-                <Client
-                    link = "#"
-                    image = {client5}
-                    imageAlt = "client5 thumbnail"
-                />
-            </div>
-        </div>
-    </div>
-);
-
-const Client = (props) => (
-    <div className="single-brand">
-        <a href={props.link}>
-            <img src={props.image} alt={props.imageAlt}/>
-        </a>
-    </div>
-);
-
-export default ClientsSection;
+// const Client = (props) => (
+//     <div className="single-brand">
+//         <a href={props.link}>
+//             <img src={props.image} alt={props.imageAlt}/>
+//         </a>
+//     </div>
+// );
+//
+// export default ClientsSection;
